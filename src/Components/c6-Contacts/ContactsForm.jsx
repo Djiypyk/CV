0cd44005b6fbf93dc3bcbb126f8b2eab94a04d79
Mikalai Zarazaka <golmtz@gmail.com>
2022-03-26 21:12:25 +0300

    add handler form

--- src/Components/c6-Contacts/ContactsForm.jsx
+++ src/Components/c6-Contacts/ContactsForm.jsx
@@ -1,9 +1,36 @@
 import React from "react";
 import styles from './ContactsForm.module.css'
 import containerStyles from './../../common/Container.module.css'
+import {useFormik} from "formik";
 
 export function ContactsForm() {
 
+    const formik = useFormik({
+        validate: (values) => {
+            const errors = {};
+            if (!values.email) {
+                errors.email = 'Required';
+            } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+                errors.email = 'Invalid email address';
+            }
+            if (!values.name) {
+                errors.name = 'Required';
+            } else if (values.name.length < 5) {
+                errors.name = 'Invalid name';
+            }
+        },
+        initialValues: {
+            name: '',
+            email: '',
+            textarea: '',
+        },
+        onSubmit: (values) => {
+            alert(JSON.stringify(values, null, 2));
+            console.log(values)
+            formik.resetForm()
+            ;
+        },
+    })
 
     return (
         <div id={'contacts'} className={styles.formWrapper}>
@@ -11,19 +38,26 @@ export function ContactsForm() {
                 <div className={styles.formContent}>
                     <h2>Contacts</h2>
                     <div className={styles.formItemWrapper}>
-                        <form>
-                            <input placeholder={'Name'} className={styles.inputForm} type="text" id='name'
-                                   required/>
-                            <br/>
-                            <input placeholder={'Email'} className={styles.inputForm} type="email" id='email'
-                                   required/>
-                            <textarea placeholder={'Write your message'} className={styles.textAreaFrom}
-                                      required
-                                      name="RecruiterMessage" id="textAreaForm"
+                        <form onSubmit={formik.handleSubmit}>
+                            <input placeholder={'Name'}
+                                   className={styles.inputForm}
+                                   type="text"
+                                   {...formik.getFieldProps('name')}
                             />
+                            <br/>
+                            <input placeholder={'Email'}
+                                   className={styles.inputForm}
+                                   type="text"
+                                   {...formik.getFieldProps('email')}
+                            />
+                            <textarea placeholder={'Write your message'}
+                                      className={styles.textAreaFrom}
+                                      {...formik.getFieldProps('textarea')}
+                            />
+                            <button className={styles.sendButton} type="submit">Send Message</button>
                         </form>
                     </div>
-                    <input className={styles.sendButton} type="submit" value={'Send Message'}/>
+
                 </div>
             </div>
         </div>
