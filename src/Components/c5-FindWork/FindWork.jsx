e2ccb1f4c1c274f23d455dcc5fad9d6469a1ac7b
Mikalai Zarazaka <golmtz@gmail.com>
2022-04-04 22:28:58 +0300

    change descriptions FindWork

--- src/Components/c5-FindWork/FindWork.jsx
+++ src/Components/c5-FindWork/FindWork.jsx
@@ -8,11 +8,18 @@ export function FindWork() {
         <div className={styles.findWorkWrapper}>
             <div className={containerStyles.container}>
                 <div className={styles.findWorkItem}>
-                    <h2>Looking for a job:</h2>
-                    <div>
-                        <p>Office. With the possibility of relocation.</p>
-                        <p>Or</p>
-                        <p>Remote Work</p>
+                    <h2>Looking for a job</h2>
+                    <div className={styles.jobDescriptions}>
+                        <div className={styles.aboutWork}>
+                            <p>I am looking for work in the office or remotely. </p>
+                            <p> It is possible to move to another city, country.</p>
+                        </div>
+                        <div className={styles.list}>
+                            <span>Important qualities of work: </span>
+                                <p>A company that wants to grow.</p>
+                                <p>The company values its employees.</p>
+                                <p>Friendly, non-toxic staff.</p>
+                        </div>
                     </div>
                 </div>
             </div>
